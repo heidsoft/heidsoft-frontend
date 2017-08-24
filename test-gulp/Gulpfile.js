102e4d4f1dcae1b51f15578062b346735143195d
heidsoft <heidsoft@qq.com>
2017-08-24 15:41:46 +0800

    Update 测试通过gulp 追加js 版本号

---
+++ test-gulp/Gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var rev = require('gulp-rev-append');
+var htmlreplace = require('gulp-html-replace');
+var strReplace  = require('gulp-string-replace');
+
+gulp.task('myreplace', function() {
+  gulp.src('views/index.html')
+    .pipe(htmlreplace({js: {
+      src: null,
+    tpl: '<script src="%f".js></script>'
+  }}))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('testRev', function () {
+    gulp.src('views/index.html')
+        .pipe(rev())
+        .pipe(strReplace('../js', '/js'))
+        .pipe(gulp.dest('views/dist'));
+});
+gulp.task('default', function() {
+    gulp.start('myreplace');
+});
